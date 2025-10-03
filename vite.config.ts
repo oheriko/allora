bc1e6378317bf2e975d2c80a8e4cab05c9e888a6
Erik Wright <yo@oheriko.com>
2025-10-03 15:10:10 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,50 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  if (mode === "client") {
-    return {
-      plugins: [
-        honox({
-          client: { input: ["/app/client.ts", "/app/style.css"] },
-        }),
-        tailwindcss(),
-      ],
-      build: {
-        outDir: "./dist/static",
-        emptyOutDir: false,
-        rollupOptions: {
-          input: ["/app/client.ts"],
-          output: {
-            entryFileNames: "static/[name]-[hash].js",
-            assetFileNames: "static/[name]-[hash].[ext]",
-          },
-        },
-      },
-    };
-  } else {
-    return {
-      plugins: [
-        honox(),
-        devServer({
-          adapter,
-          entry: "app/server.ts",
-        }),
-        tailwindcss(),
-      ],
-      ssr: {
-        external: ["hono"],
-      },
-      build: {
-        ssr: true,
-        outDir: "./dist",
-        rollupOptions: {
-          input: "./app/server.ts",
-          output: {
-            entryFileNames: "[name].js",
-          },
-        },
-      },
-    };
-  }
+export default defineConfig({
+  plugins: [
+    honox(),
+    devServer({
+      adapter,
+      entry: "app/server.ts",
+    }),
+    tailwindcss(),
+  ],
 });
